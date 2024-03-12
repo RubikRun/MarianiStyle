b39beb491d944ca9f9d4c5f68bd1c96149585dd8
Boris Dimitrov <borisd2000@gmail.com>
2024-03-12 15:52:15 +0200

    add employee names above tables by creating ScheduleEmployeesWidget

--- schedule_widget.py
+++ schedule_widget.py
@@ -1,23 +1,23 @@
 from schedule import Schedule
 from PySide6.QtCore import Qt
 from PySide6.QtCore import QDate
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QLabel
 from PySide6.QtGui import QFont
 
-class ScheduleWidget(QWidget):
-    def __init__(self):
+schedule_font = QFont("Verdana", 14)
+
+class ScheduleTablesWidget(QWidget):
+    def __init__(self, schedule, employees):
         super().__init__()
+        self.schedule = schedule
+        self.employees = employees
+
         self.font_size = 14
         self.tables_items_count = {}
 
-        # Example schedule
-        self.schedule = Schedule()
-        self.schedule.load_example_data()
-
-        self.date = QDate(2024, 4, 9)
-        self.employees = self.schedule.get_employees(self.date)
-
         self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(10, 0, 10, 10)
+        self.layout.setSpacing(20)
         # Create tables
         self.tables = {}
         # Traverse employees
@@ -40,11 +40,11 @@ class ScheduleWidget(QWidget):
                 QHeaderView::section {
                     background-color: #606060;
                     color: white;
-                    font-size: """ + str(self.font_size) + """pt;
+                    font-size: """ + str(schedule_font.pointSize()) + """pt;
                 }
             """)
             # Change the font of the table
-            table.setFont(QFont("Verdana", self.font_size))
+            table.setFont(schedule_font)
             # Add table to layout
             self.layout.addWidget(table)
             # Add table to tables member
@@ -57,7 +57,7 @@ class ScheduleWidget(QWidget):
     def fill_tables(self, schedule=None):
         schedule = self.schedule if not schedule else schedule
         # Traverse employees and their reservations for current date
-        for employee, reservations in schedule.data[self.date].items():
+        for employee, reservations in schedule.items():
             table = self.tables[employee]
             # Traverse current employee's reservations
             for time_interval, client in reservations.items():
@@ -68,3 +68,35 @@ class ScheduleWidget(QWidget):
                 client_widget_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
                 table.setItem(self.tables_items_count[employee], 1, client_widget_item)
                 self.tables_items_count[employee] += 1
+            table.setRowCount(self.tables_items_count[employee])
+
+class ScheduleEmployeesWidget(QWidget):
+    def __init__(self, employees):
+        super().__init__()
+        self.layout = QHBoxLayout(self)
+        self.layout.setContentsMargins(10, 10, 10, 0)
+        self.layout.setSpacing(20)
+        for employee in employees:
+            label = QLabel(employee)
+            label.setFont(schedule_font)
+            # Uncomment to make border visible
+            #label.setStyleSheet("border: 2px solid black; padding: 5px;")
+            self.layout.addWidget(label)
+
+class ScheduleWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.schedule = Schedule()
+        self.schedule.load_example_data()
+
+        self.date = QDate(2024, 4, 9)
+        self.employees = self.schedule.get_employees(self.date)
+
+        self.employees_widget = ScheduleEmployeesWidget(self.employees)
+        self.tables_widget = ScheduleTablesWidget(self.schedule.data[self.date], self.employees)
+        self.layout = QVBoxLayout(self)
+        self.layout.setSpacing(0)
+        self.layout.addWidget(self.employees_widget)
+        self.layout.addWidget(self.tables_widget)
+
