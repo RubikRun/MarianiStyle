f78db31d7dcfe3b1cc35441199a7d87626ef4770
Boris Dimitrov <borisd2000@gmail.com>
2024-03-12 13:30:30 +0200

    make time intervals be a column instead of vertical header

--- schedule_widget.py
+++ schedule_widget.py
@@ -1,4 +1,5 @@
 from schedule import Schedule
+from PySide6.QtCore import Qt
 from PySide6.QtCore import QDate
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView
 from PySide6.QtGui import QFont
@@ -19,12 +20,17 @@ class ScheduleWidget(QWidget):
         self.layout = QHBoxLayout(self)
         # Create tables
         self.tables = {}
+        # Traverse employees
         for employee in self.employees:
+            # Create a table for each employee
             table = QTableWidget()
-            table.setColumnCount(1)
-            table.setHorizontalHeaderLabels(["Клиент"])
-            table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-            # Set styles for the table
+            # Handle properties of rows and columns and their headers
+            table.setColumnCount(2)
+            table.setHorizontalHeaderLabels(["Час", "Клиент"])
+            table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+            table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+            table.verticalHeader().hide()
+            # Set style sheet for the table
             table.setStyleSheet("""
                 QTableWidget {
                     background-color: #f0f0f0;
@@ -39,24 +45,26 @@ class ScheduleWidget(QWidget):
             """)
             # Change the font of the table
             table.setFont(QFont("Verdana", self.font_size))
-
-            # add table to layout
+            # Add table to layout
             self.layout.addWidget(table)
+            # Add table to tables member
             self.tables[employee] = table
             self.tables_items_count[employee] = 0
 
-        # Fill example data
+        # Fill tables with data
         self.fill_tables()
 
     def fill_tables(self, schedule=None):
         schedule = self.schedule if not schedule else schedule
+        # Traverse employees and their reservations for current date
         for employee, reservations in schedule.data[self.date].items():
             table = self.tables[employee]
-            vertical_header_labels = []
+            # Traverse current employee's reservations
             for time_interval, client in reservations.items():
                 table.insertRow(self.tables_items_count[employee])
-                table.setItem(self.tables_items_count[employee], 0, QTableWidgetItem(client))
                 time_str = time_interval.time_begin.toString("HH:mm") + " - " + time_interval.time_end.toString("HH:mm")
-                vertical_header_labels.append(time_str)
+                table.setItem(self.tables_items_count[employee], 0, QTableWidgetItem(time_str))
+                client_widget_item = QTableWidgetItem(client)
+                client_widget_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+                table.setItem(self.tables_items_count[employee], 1, client_widget_item)
                 self.tables_items_count[employee] += 1
-            table.setVerticalHeaderLabels(vertical_header_labels)
