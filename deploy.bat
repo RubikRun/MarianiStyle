cb8ce2260c58f70a5aae0530d1a193588040f835
Boris Dimitrov <borisd2000@gmail.com>
2024-03-28 11:57:04 +0200

    update deploy script, update exe

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
-pyinstaller --hidden-import PySide2 --add-data "icons/date_arrow_left.png;." mainwindow.py
-xcopy icons dist\mainwindow\icons /E /I
-xcopy database dist\mainwindow\database /E /I
+pyinstaller --hidden-import PySide2 --add-data "icons/date_arrow_left.png;." main_window.py
+xcopy icons dist\main_window\icons /E /I
+xcopy database dist\main_window\database /E /I
