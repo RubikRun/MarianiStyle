3e28c9314e56d0a3d28f0bf6b981ed9a495dcc56
Boris Dimitrov <borisd2000@gmail.com>
2024-04-02 16:04:49 +0300

    add class DataIO for handling input/output of multiple-component data

---
+++ database/data_io.py
@@ -0,0 +1,66 @@
+from logger import Logger
+
+class DataIO:
+    # Parses a data declaration string.
+    # The string should be of format "part0;part1;part2;part3".
+    # The type of each part should be specified with the types list/string.
+    # It's a list/string of same length as the number of parts. Each element of the list/string is either 's', 'i' or 'f'
+    # meaning that the corresponding part will be parsed as a string, int or float.
+    # The function returns a list of parsed values, one for each part.
+    # If parsing of some part failed the result for that part will be None.
+    def parse_declaration(decl, types):
+        for type in types:
+            if type not in ['s', 'i', 'f']:
+                Logger.log_error("Invalid types given when parsing a data declaration. Declaration will be skipped.")
+                return None
+        parts = decl.split(';')
+        if len(parts) != len(types):
+            Logger.log_error("Data declaration has different number of parts and types. Declaration will be skipped.")
+        
+        results = []
+        for idx, part in enumerate(parts):
+            type = types[idx]
+            if type == 's':
+                results.append(part)
+            elif type == 'i':
+                try:
+                    result = int(part)
+                except ValueError:
+                    Logger.log_error("Part {} of a data declaration is not an integer. This part will be None.")
+                    result = None
+                results.append(result)
+            elif type == 'f':
+                try:
+                    result = float(part)
+                except ValueError:
+                    Logger.log_error("Part {} of a data declaration is not a float. This part will be None.")
+                    result = None
+                results.append(result)
+                
+        return results
+
+    # Parses a variable assignment string.
+    # The string should be of format "$VARIABLE_NAME=VARIABLE_VALUE"
+    # where VARIABLE_NAME should match the var_name parameter here
+    # and VARIABLE_VALUE will be parsed as specified in the type parameter.
+    # Type can be either 's', 'i' or 'f' for string, int or float.
+    # If the variable assignment is not in the given format or the variable name doesn't match,
+    # or the parsing fails, None will be returned.
+    # Otherwise the parsed value will be returned.
+    def parse_variable_assignment(asgn, var_name, type):
+        if asgn.startswith('$' + var_name + '='):
+            value_str = asgn[len('$' + var_name + '='):]
+            if type == 's':
+                return value_str
+            elif type == 'i':
+                try:
+                    value = int(value_str)
+                    return value
+                except ValueError:
+                    Logger.log_error("Value in a variable assignment is not an integer. Assignment will be skipped.")
+            elif type == 'f':
+                try:
+                    value = float(value_str)
+                    return value
+                except ValueError:
+                    Logger.log_error("Value in a variable assignment is not a float. Assignment will be skipped.")
