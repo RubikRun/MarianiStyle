8701cc26c13decc6c64310a419f9217418da623e
Boris Dimitrov <borisd2000@gmail.com>
2024-04-02 14:47:47 +0300

    delete everything, start again

--- README.md
+++
@@ -1,2 +0,0 @@
-Business Management System for a beauty salon in my hometown Burgas, Bulgaria.
-Made in Qt with Python
--- deploy.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller --hidden-import PySide2 --add-data "icons/date_arrow_left.png;." main_window.py
-xcopy icons dist\main_window\icons /E /I
-xcopy database dist\main_window\database /E /I
