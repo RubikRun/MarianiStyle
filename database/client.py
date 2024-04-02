c95fbe30d80f647296bdde49e77917e88f9f65e3
Boris Dimitrov <borisd2000@gmail.com>
2024-04-02 14:57:38 +0300

    add class Client

---
+++ database/client.py
@@ -0,0 +1,6 @@
+class Client:
+    def __init__(self, id, name, phone, packet_instances):
+        self.id = id
+        self.name = name
+        self.phone = phone
+        self.packet_instances = packet_instances
