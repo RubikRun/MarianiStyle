79a94610528950c37323e78f1262cf3ab8a97039
Boris Dimitrov <borisd2000@gmail.com>
2024-04-02 14:56:35 +0300

    add class PacketInstance

---
+++ database/packet_instance.py
@@ -0,0 +1,8 @@
+class PacketInstance:
+    def __init__(self, id, packet_id, client_id, employee_id, bought_on, use_count):
+        self.id = id
+        self.packet_id = packet_id
+        self.client_id = client_id
+        self.employee_id = employee_id
+        self.bought_on = bought_on
+        self.use_count = use_count
