1a53fbe3b39e3aec416584a6bb3df6193fb86490
Boris Dimitrov <borisd2000@gmail.com>
2024-04-02 14:55:09 +0300

    add class Packet

---
+++ database/packet.py
@@ -0,0 +1,7 @@
+class Packet:
+    def __init__(self, id, name, price, uses, validity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.uses = uses
+        self.validity = validity
