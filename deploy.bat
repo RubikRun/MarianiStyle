a3c0146155b7210a345af9f6ea2718cc76b68373
Boris Dimitrov <borisd2000@gmail.com>
2024-04-06 04:17:34 +0300

    deploy VERSION_1

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
-pyinstaller --hidden-import PySide2 --add-data "icons/date_arrow_left.png;." main.py
+pyinstaller --noconsole --hidden-import PySide2 --add-data "icons/date_arrow_left.png;." main.py
 xcopy icons dist\main\icons /E /I
 xcopy data dist\main\data /E /I
+xcopy config dist\main\config /E /I
